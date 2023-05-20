f41bac8883c1c6822faeef1a5733caabb118781d
Thyago Ramos <oficialthyago2@gmail.com>
2023-05-20 18:45:25 -0300

    Criação de classas e metodos novos para finalizão da logica de classe devs e bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+ //------------------------------------------------------
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição do curso java ");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso java");
+        curso2.setDescricao("Descrição do curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("Mentoria curso java");
+        mentoria.setData(LocalDate.now());
+
+
+//-----------------------------------------------------------
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
     }
 
 }
